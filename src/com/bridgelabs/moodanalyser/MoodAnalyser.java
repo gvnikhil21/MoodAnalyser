ddb4d27bffdc04569cb2b2fd4ac96c4359a79f62
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-06 20:15:28 +0530

    Added: Respond happy or sad mood to a given message functionality added

--- src/com/bridgelabs/moodanalyser/MoodAnalyser.java
+++ src/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -4,5 +4,13 @@ public class MoodAnalyser {
 	public static void main(String args[]) {
 		// welcome message
 		System.out.println("Welcome to Mood Analyser!");
+		MoodAnalyser moodAnalyser = new MoodAnalyser();
+		String mood = moodAnalyser.analyseMoode("I am in Happy mood");
+		System.out.println("My mood is " + mood);
+	}
+
+	// analyse sad or happy mood
+	public String analyseMoode(String message) {
+		return message.contains("Happy") ? "HAPPY" : "SAD";
 	}
 }
