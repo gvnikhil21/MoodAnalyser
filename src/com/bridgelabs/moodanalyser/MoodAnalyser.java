9432c30b190e3106138b8bdfd471e3a60d1c17f6
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-06 20:09:33 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabs/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,8 @@
+package com.bridgelabs.moodanalyser;
+
+public class MoodAnalyser {
+	public static void main(String args[]) {
+		// welcome message
+		System.out.println("Welcome to Mood Analyser!");
+	}
+}
